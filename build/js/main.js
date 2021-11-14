02e65843737a6dca7ed6125404d5c21135a68d46
Iskandar <iskandjuma@gmail.com>
2021-11-14 22:57:36 +0300

    добавляет исправления от наставника

--- build/js/main.js
+++ build/js/main.js
@@ -38,8 +38,8 @@
   }
 
   var modalLink = document.querySelector('.main-nav__button');
-  var feedbackPopup = document.querySelector('.modal__overlay');
-  var modalClose = document.querySelector('.modal-close');
+  var feedbackPopup = document.querySelector('.modal-overlay');
+  var modalClose = document.querySelector('.modal__close');
   var userName = document.querySelector('input[data-text-input]');
   var modalForm = document.querySelector('.modal__form');
   var userPhone = document.querySelector('input[data-tel-input]');
@@ -53,37 +53,42 @@
     isStorageSupport = false;
   }
 
-
-  modalLink.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    feedbackPopup.classList.add('modal__show');
-
-    if (storage) {
-      userName.value = storage;
-      userPhone.focus();
-    } else {
-      userName.focus();
-    }
-  });
-
-  modalClose.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    feedbackPopup.classList.remove('modal__show');
-    feedbackPopup.classList.remove('modal__error');
-  });
-
-  modalForm.addEventListener('submit', function (evt) {
-    if (!userName.value || !userPhone.value) {
+  if (modalLink) {
+    modalLink.addEventListener('click', function (evt) {
       evt.preventDefault();
-      feedbackPopup.classList.remove('modal__error');
-      feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
-      feedbackPopup.classList.add('modal__error');
-    } else {
-      if (isStorageSupport) {
-        localStorage.setItem('login', userName.value);
+      feedbackPopup.classList.add('modal__show');
+
+      if (storage) {
+        userName.value = storage;
+        userPhone.focus();
+      } else {
+        userName.focus();
       }
-    }
-  });
+    });
+  }
+
+  if (modalClose) {
+    modalClose.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      feedbackPopup.classList.remove('modal__show');
+      feedbackPopup.classList.remove('modal__error');
+    });
+  }
+
+  if (modalForm) {
+    modalForm.addEventListener('submit', function (evt) {
+      if (!userName.value || !userPhone.value) {
+        evt.preventDefault();
+        feedbackPopup.classList.remove('modal__error');
+        feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+        feedbackPopup.classList.add('modal__error');
+      } else {
+        if (isStorageSupport) {
+          localStorage.setItem('login', userName.value);
+        }
+      }
+    });
+  }
 
   window.addEventListener('keydown', function (evt) {
     if (evt.key === 'Escape') {
@@ -95,62 +100,62 @@
     }
   });
 
+  var phoneInputs = document.querySelectorAll('input[data-tel-input]');
+
+  if (phoneInputs) {
+    var getInputNumbersValue = function (input) {
+      return input.value.replace(/\D/g, '');
+    };
+
+    var onPhoneInput = function (e) {
+      var input = e.target;
+      var inputNumbersValue = getInputNumbersValue(input);
+      var formattedInputValue = '';
+
+      if (!inputNumbersValue) {
+        input.value = '';
+        return;
+      }
+
+      if (['7', '8', '9'].indexOf(inputNumbersValue[0]) > -1) {
+        if (inputNumbersValue[0] === '9') {
+          inputNumbersValue = '7' + inputNumbersValue;
+        }
+        var firstSymbols = (inputNumbersValue[0] === '8') ? '8' : '+7';
+        formattedInputValue = firstSymbols + ' ';
+        if (inputNumbersValue.length > 1) {
+          formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+        }
+        if (inputNumbersValue.length >= 5) {
+          formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+        }
+        if (inputNumbersValue.length >= 8) {
+          formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+        }
+        if (inputNumbersValue.length >= 10) {
+          formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+        }
+      } else {
+        // not russian
+        formattedInputValue = '+7' + inputNumbersValue.substring(0, 10);
+      }
+      input.value = formattedInputValue;
+    };
+
+    var onPhoneKeyDown = function (e) {
+      var input = e.target;
+      if (e.keyCode === 8 && getInputNumbersValue(input).length === 1) {
+        input.value = '';
+      }
+    };
+
+    for (var i = 0; i < phoneInputs.length; i++) {
+      var input = phoneInputs[i];
+      input.addEventListener('focus', function (evt) {
+        evt.target.value = '+7 (';
+      });
+      input.addEventListener('input', onPhoneInput);
+      input.addEventListener('keydown', onPhoneKeyDown);
+    }
+  }
 })();
-
-
-var phoneInputs = document.querySelectorAll('input[data-tel-input]');
-
-var getInputNumbersValue = function (input) {
-  return input.value.replace(/\D/g, '');
-};
-
-var onPhoneInput = function (e) {
-  var input = e.target;
-  var inputNumbersValue = getInputNumbersValue(input);
-  var formattedInputValue = '';
-
-  if (!inputNumbersValue) {
-    input.value = '';
-    return;
-  }
-
-  if (['7', '8', '9'].indexOf(inputNumbersValue[0]) > -1) {
-    if (inputNumbersValue[0] === '9') {
-      inputNumbersValue = '7' + inputNumbersValue;
-    }
-    var firstSymbols = (inputNumbersValue[0] === '8') ? '8' : '+7';
-    formattedInputValue = firstSymbols + ' ';
-    if (inputNumbersValue.length > 1) {
-      formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
-    }
-    if (inputNumbersValue.length >= 5) {
-      formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
-    }
-    if (inputNumbersValue.length >= 8) {
-      formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
-    }
-    if (inputNumbersValue.length >= 10) {
-      formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
-    }
-  } else {
-    // not russian
-    formattedInputValue = '+7' + inputNumbersValue.substring(0, 10);
-  }
-  input.value = formattedInputValue;
-};
-
-var onPhoneKeyDown = function (e) {
-  var input = e.target;
-  if (e.keyCode === 8 && getInputNumbersValue(input).length === 1) {
-    input.value = '';
-  }
-};
-
-for (var i = 0; i < phoneInputs.length; i++) {
-  var input = phoneInputs[i];
-  input.addEventListener('focus', function (evt) {
-    evt.target.value = '+7 (';
-  });
-  input.addEventListener('input', onPhoneInput);
-  input.addEventListener('keydown', onPhoneKeyDown);
-}
